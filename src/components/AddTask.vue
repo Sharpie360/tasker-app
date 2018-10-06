5896020f072276af6a682ab42faf1293558cc0ee
SeanBecker <techtronixwebdesign@gmail.com>
2018-10-06 17:42:25 -0500

    added - AddTask cmp

---
+++ src/components/AddTask.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="input-control">
+    <h4>Add a Task</h4>
+    <input type="text" name="" id="" v-model="newTask">
+    <button 
+      id="add" 
+      @click="addTask()">
+      Add New Task
+    </button>
+  </div>
+</template>
+
+<script>
+  import { eventBus } from '../main.js'
+
+  export default {
+    data () {
+      return {
+        newTask: ''
+      }
+    },
+    methods: {
+      addTask() {
+        const taskToAdd = this.newTask
+        eventBus.$emit('newTaskAdded', taskToAdd)
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
