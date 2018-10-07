5a9100f628ab629cdfd32648a42b618836dc2b10
SeanBecker <techtronixwebdesign@gmail.com>
2018-10-07 00:27:15 -0500

    added - TaskDetails cmp

---
+++ src/components/TaskDetails.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="details card-body">
+    <div class="details-steps">
+      <ul>
+        <li v-for="(step, i) in steps" :key="step">
+          {{i + 1}}. {{ step }}
+        </li>
+
+      </ul>
+    </div>
+    <div class="details-info">
+      <p >Contact Info: {{ contact }}</p>
+      <p >Due Date: {{ due }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+  import TaskList from './TaskList.vue'
+  export default {
+    data () {
+      return {
+
+      }
+    },
+    props: {
+      due: String,
+      contact: String,
+      steps: []
+    }
+  }
+</script>
+
+<style scoped>
+
+.details {
+  display: grid;
+  grid-template-columns: 3fr 2fr;
+  padding-left: 0;
+}
+
+.details > div {
+  border: 1px solid #ccc
+}
+
+li {
+  margin-left: -1rem;
+  list-style: none;
+}
+p {
+  margin-left: 1rem;
+}
+</style>
