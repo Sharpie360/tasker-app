ae7a97427756c6e3e78debea8588fbc69b565558
SeanBecker <techtronixwebdesign@gmail.com>
2018-10-21 22:13:37 -0500

    updated - important/optional step logic and color transition, isImportantStep method

--- src/components/AddTask.vue
+++ src/components/AddTask.vue
@@ -16,7 +16,9 @@
       </h5>
       
       <div class="card-body" v-show="newTask.addDetails_detailsShown">
-        <div class="grid">
+        <div 
+          class="grid"
+          :class="mediaQueries">
           <div class="form-group">
             <label for="due-date">Due Date</label>
             <input class="form-control" type="date" id="due-date" v-model="newTask.due">
@@ -39,7 +41,7 @@
           </h5>
           <div class="card-body p-0">
             <ul class="list-group list-group-flush" id="steps-list">
-              <li class="list-group-item py-2" v-for="(step, i) in newTask.steps" :key="i">
+              <li class="list-group-item py-2 pr-1" v-for="(step, i) in newTask.steps" :key="i">
                 <div class="form-group step-group mb-0">
                   <!-- add editmode with vshow -->
                   <label 
@@ -56,20 +58,34 @@
 
                   <div class="step-group--actionBtns">
                       <div 
-                        class="important-outer" 
+                        class="action-btn-outer" 
                         @click="setAsImportantStep(i)"> 
-                        <!-- <img src="../assets/exclamation-solid.svg" alt=""
-                        class="actionBtn actionBtn-icon"
-                        > -->
                         <svg 
-                          class="step-important-svg" 
-                          
+                          class="step-important-svg"  
                           xmlns="http://www.w3.org/2000/svg" 
                           viewBox="0 0 192 512">
+                            <!-- Dynamic Styling of the ! icon fixed!!! vvvvv -->
                             <path 
                               class="important-svg-path"
+                              :class="{ 'important-true': newTask.steps[i].isImportantStep }"
                               fill="currentColor" 
                               d="M176 432c0 44.112-35.888 80-80 80s-80-35.888-80-80 35.888-80 80-80 80 35.888 80 80zM25.26 25.199l13.6 272C39.499 309.972 50.041 320 62.83 320h66.34c12.789 0 23.331-10.028 23.97-22.801l13.6-272C167.425 11.49 156.496 0 142.77 0H49.23C35.504 0 24.575 11.49 25.26 25.199z">
+                            </path>
+                        </svg>
+                      </div>
+                      <div 
+                        class="action-btn-outer"
+                        @click="setAsOptionalStep(i)"
+                        >
+                        <svg 
+                          class="step-optional-svg" 
+                          xmlns="http://www.w3.org/2000/svg" 
+                          viewBox="0 0 384 512">
+                            <path 
+                              class="optional-svg-path"
+                              :class="{ 'optional-true': newTask.steps[i].isOptionalStep }"
+                              fill="currentColor" 
+                              d="M202.021 0C122.202 0 70.503 32.703 29.914 91.026c-7.363 10.58-5.093 25.086 5.178 32.874l43.138 32.709c10.373 7.865 25.132 6.026 33.253-4.148 25.049-31.381 43.63-49.449 82.757-49.449 30.764 0 68.816 19.799 68.816 49.631 0 22.552-18.617 34.134-48.993 51.164-35.423 19.86-82.299 44.576-82.299 106.405V320c0 13.255 10.745 24 24 24h72.471c13.255 0 24-10.745 24-24v-5.773c0-42.86 125.268-44.645 125.268-160.627C377.504 66.256 286.902 0 202.021 0zM192 373.459c-38.196 0-69.271 31.075-69.271 69.271 0 38.195 31.075 69.27 69.271 69.27s69.271-31.075 69.271-69.271-31.075-69.27-69.271-69.27z">
                             </path>
                         </svg>
                       </div>
@@ -139,6 +155,14 @@
           'alert-danger': !this.alertMsg.isSuccess
         }
       },
+      mediaQueries(){
+        // not quite finished! fix these! 
+        return {
+          // 'grid--mobile': this.$mq === 'mobile',
+          // 'grid': !this.mq === 'mobile'
+        }
+
+      }
     },
     methods: {
       addNewTask() {
@@ -163,7 +187,8 @@
         if(this.newTask.steps.length === 0){
             this.newTask.steps.push({
             value: '',
-            isImportantStep: false
+            isImportantStep: false,
+            isOptionalStep: false
           })
         } else if (this.newTask.steps[this.newTask.steps.length - 1].value === ''){
           console.log('empty string')
@@ -172,7 +197,8 @@
         } else {
           this.newTask.steps.push({
             value: '',
-            isImportantStep: false
+            isImportantStep: false,
+            isOptionalStep: false
           })
         }
       },
@@ -181,10 +207,11 @@
         // console.log('before: ', this.newTask.steps[currentStep].isImportantStep)
         this.newTask.steps[currentStep].isImportantStep = !this.newTask.steps[currentStep].isImportantStep
         // console.log('after ', this.newTask.steps[currentStep].isImportantStep, this.showIf_isImportant)
-        const svgPath = document.querySelectorAll('.important-svg-path')
-        const pathArr = Array.from(svgPath)
-        console.log(pathArr)
-
+      },
+      setAsOptionalStep(i) {
+        console.log('test')
+        this.newTask.steps[i].isOptionalStep = !this.newTask.steps[i].isOptionalStep
+        console.log('new val: ', this.newTask.steps[i].isOptionalStep)
       },
       deleteStep(i){
         console.log('deleteClicked', i)
@@ -207,6 +234,8 @@
 </script>
 
 <style scoped>
+
+
 .add-details-header {
   display: flex;
   flex: 1;
@@ -223,6 +252,12 @@
   grid-template-columns: 1fr 1fr;
   grid-gap: 1rem;
 }
+/* .grid--mobile {
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-gap: 1rem;
+    /* not quite finished! fix these! *
+} */
 
 #add-new-step-btn {
   font-size: .9rem;
@@ -250,23 +285,26 @@
   height: 1.5rem;
   width: 1.5rem;
 }
-.important-outer {
+.action-btn-outer {
   width: 2rem;
   display: flex;
   justify-content: center;
   align-items: center;
 }
-.step-important-svg {
+.step-important-svg, .step-optional-svg {
   flex: 1;
   height: 1.75rem;
   width: 1.75rem
 }
-.important-svg-path {
+.important-svg-path, .optional-svg-path {
   fill: #000;
   transition: fill 0.1s ease-in-out;
 }
 .step-important-svg .important-true {
   fill: #00D6A4;
+}
+.step-optional-svg .optional-true {
+  fill: #006473;
 }
 
 .step-remove {
@@ -281,4 +319,5 @@
 #alert-box {
   flex: 1;
 }
+
 </style>
