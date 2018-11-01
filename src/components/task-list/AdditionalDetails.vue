e074a8805945bee36ed02705fbcd93ff43688b13
SeanBecker <techtronixwebdesign@gmail.com>
2018-10-31 23:24:11 -0500

    added - Additional Details child cmp with props

---
+++ src/components/task-list/AdditionalDetails.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="details-info card">
+    <h5 class="card-header addtl-details-title">Additional Details</h5>
+    <div class="card-body">
+      <p class="card-text">Contact Info: {{ contact }}</p>
+      <p class="card-text">Due Date: {{ due }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['due', 'contact']
+}
+</script>
+
+<style>
+.addtl-details-title {
+  background-color: #03D8CB;
+  font-weight: 300;
+  font-size: 1.35rem;
+  padding: 1rem;
+  border-bottom: 1px solid rgba(33, 33, 33, .2);
+}
+.card {
+  border: 1px solid rgba(33, 33, 33, .4);
+}
+</style>
