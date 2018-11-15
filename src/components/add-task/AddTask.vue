fe7e26816d4441fb7ff1cec573e6213643e85ff7
SeanBecker <techtronixwebdesign@gmail.com>
2018-11-14 23:14:52 -0600

    removed - display wrapper div for view toggle

--- src/components/add-task/AddTask.vue
+++ src/components/add-task/AddTask.vue
@@ -1,59 +1,59 @@
 <template>
   <div class="input-control form-group mb-0">
-    <div class="display-wrapper" v-show="!showCompletedTasksCmp">
-      <input 
-        type="text" 
-        placeholder="new task title"
-        id="addInput" 
-        class="form-control mb-3" 
-        v-model="newTask.task"
-      >
-      <div class="card">
-        <h5 class="card-header add-details-header">Add Additional Details
-          <span 
-            class="add-details-expander" 
-            v-on:click="showAddDetailsCmp">+</span>
-        </h5>
-        
-        <div class="card-body" v-show="newTask.addDetails_detailsShown">
-          <div 
-            class="grid"
-            :class="mediaQueries">
-            <div class="form-group">
-              <label for="due-date">Due Date</label>
-              <input class="form-control" type="date" id="due-date" v-model="newTask.due">
-            </div>
-            <div class="form-group">
-              <label for="contact">Contact Info</label>
-              <input class="form-control" type="text" id="contact" v-model="newTask.contact">
-            </div>
-          </div> <!-- .grid end -->
-
-          <app-add-steps></app-add-steps>
-
-        </div>
-      </div>
-      <div id="add-task-btn-div" class="mt-3">
-        <button 
-          id="add" 
-          class="btn btn-info pointer"
-          @click="addNewTask()">
-          <h5 class="mb-0">Add New Task</h5>
-        </button>
+    <h2 class="shadow-white">Add Task</h2>
+    <input 
+      type="text" 
+      placeholder="new task title"
+      id="addInput" 
+      class="form-control mb-3" 
+      v-model="newTask.task"
+    >
+    <div class="card">
+      <h5 class="card-header add-details-header">Add Additional Details
+        <span 
+          class="add-details-expander" 
+          v-on:click="showAddDetailsCmp">+</span>
+      </h5>
+      
+      <div class="card-body" v-show="newTask.addDetails_detailsShown">
         <div 
-          id="alert-box" 
-          class="alert py-2 ml-3 mb-0 text-center" 
-          :class="alertStyles"
-          v-show="alertMsg.isAlert">
-          <p 
-            class="mb-0" 
-            id="alert-msg">
-            {{ alertMsg.message }}
-          </p>
-        </div>
+          class="grid"
+          :class="mediaQueries">
+          <div class="form-group">
+            <label for="due-date">Due Date</label>
+            <input class="form-control" type="date" id="due-date" v-model="newTask.due">
+          </div>
+          <div class="form-group">
+            <label for="contact">Contact Info</label>
+            <input class="form-control" type="text" id="contact" v-model="newTask.contact">
+          </div>
+        </div> <!-- .grid end -->
+
+        <app-add-steps></app-add-steps>
+
+      </div>
+    </div>
+    <div id="add-task-btn-div" class="mt-3">
+      <button 
+        id="add" 
+        class="btn btn-info pointer"
+        @click="addNewTask()">
+        <h5 class="mb-0">Add New Task</h5>
+      </button>
+      <div 
+        id="alert-box" 
+        class="alert py-2 ml-3 mb-0 text-center" 
+        :class="alertStyles"
+        v-show="alertMsg.isAlert">
+        <p 
+          class="mb-0" 
+          id="alert-msg">
+          {{ alertMsg.message }}
+        </p>
       </div>
     </div>
   </div>
+
 </template>
 
 <script>
