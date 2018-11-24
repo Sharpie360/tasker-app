09742cc3502d5e01ec2946233ebb6ac1dcc10bf3
SeanBecker <techtronixwebdesign@gmail.com>
2018-11-23 22:41:26 -0600

    updated - editmode trigger for edit details, custom event

--- src/components/task-list/ManageButtons.vue
+++ src/components/task-list/ManageButtons.vue
@@ -5,21 +5,34 @@
         v-show="task.completed"
         @click="archiveTask(i)"
         id="task-btn--archive"
-        class="btn btn-primary mr-2 py-1">
-          Archive Task
+        class="btn btn-primary mr-2 py-1"
+        >Archive Task
       </button>
     </div>
     <div>
+      <!-- Is Not Editing -->
       <button 
+        v-if="!editingDetails"
+        @click="editDetails"
         id="task-btn--edit-details"
-        class="btn btn-info mr-2 py-1">
-        Edit Details
+        class="btn btn-info mr-2 py-1"
+        >Edit Details
       </button>
+
+      <!-- Is Editing -->
+      <button 
+        v-if="editingDetails"
+        @click="saveEdits"
+        id="task-btn--edit-details"
+        class="btn btn-info mr-2 py-1"
+        >Save Edits
+      </button>
+
       <button 
         @click="deleteTask(task, i)"
         id="task-btn--delete"
-        class="btn btn-danger mr-0 py-1">
-        Delete Task
+        class="btn btn-danger mr-0 py-1"
+        >Delete Task
       </button>
     </div>
   </div>
@@ -31,17 +44,25 @@ import { eventBus } from '../../main.js'
 export default {
   data () {
     return {
-
+      editingDetails: false
     }
   },
   props: ['task', 'i'],
   methods: {
-    deleteTask(task, i){
+    editDetails() {
+      this.editingDetails = true
+      eventBus.$emit('editDetailsCE')
+    },
+    saveEdits() {
+      this.editingDetails = false
+      eventBus.$emit('saveEditsCE')
+    },
+    deleteTask(task, i) {
       if(confirm(`Are you sure you want to delete this task? Task: ${task.task}`)) {
         eventBus.$emit('deleteTask', i)
       }
     },
-    archiveTask(i){
+    archiveTask(i) {
       eventBus.$emit('archiveTask', i)
     }
   }
