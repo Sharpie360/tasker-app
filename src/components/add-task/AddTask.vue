364b285866a590e74a1437126bb92ad761cadd7f
SeanBecker <techtronixwebdesign@gmail.com>
2018-12-04 23:25:05 -0600

    updated - animations for add details height and opacity of children, alert message

--- src/components/add-task/AddTask.vue
+++ src/components/add-task/AddTask.vue
@@ -15,31 +15,42 @@
         >Clear Form
       </button>
     </div>
-    <div class="card">
-      <h5 class="card-header add-details-header">Add Additional Details
-        <span 
-          class="add-details-expander" 
-          v-on:click="showAddDetailsCmp">+</span>
-      </h5>
-      
-      <div class="card-body" v-show="newTask.addDetails_detailsShown">
-        <div 
-          class="grid"
-          :class="mediaQueries">
-          <div class="form-group">
-            <label for="due-date">Due Date</label>
-            <input class="form-control" type="date" id="due-date" v-model="newTask.due">
-          </div>
-          <div class="form-group">
-            <label for="contact">Contact Info</label>
-            <input class="form-control" type="text" id="contact" v-model="newTask.contact">
-          </div>
-        </div> <!-- .grid end -->
 
-        <app-add-steps></app-add-steps>
+      <div class="card">
+        <h5 class="card-header add-details-header">Add Additional Details
+          <span 
+            class="add-details-expander" 
+            v-on:click="showAddDetailsCmp">+</span>
+        </h5>
+        
+        <transition 
+          name="expand" 
+          type="transition"
+          @before-enter="beforeEnter"
+          @after-enter="afterEnter"
+          @before-leave="beforeLeave"
+          :css="true"
+          >
+          <div class="card-body add-details-body" v-show="newTask.addDetails_detailsShown">
+            <div 
+              class="grid"
+              >
+              <div class="form-group">
+                <label for="due-date">Due Date</label>
+                <input class="form-control" type="date" id="due-date" v-model="newTask.due">
+              </div>
+              <div class="form-group">
+                <label for="contact">Contact Info</label>
+                <input class="form-control" type="text" id="contact" v-model="newTask.contact">
+              </div>
+            </div> <!-- .grid end -->
 
+            <app-add-steps></app-add-steps>
+
+          </div>
+        </transition>
       </div>
-    </div>
+
     <div id="add-task-btn-div" class="mt-3">
       <button 
         id="add" 
@@ -47,17 +58,21 @@
         @click="addNewTask()">
         <h5 class="mb-0">Add New Task</h5>
       </button>
-      <div 
-        id="alert-box" 
-        class="alert py-2 ml-3 mb-0 text-center" 
-        :class="alertStyles"
-        v-show="alertMsg.isAlert">
-        <p 
-          class="mb-0" 
-          id="alert-msg">
-          {{ alertMsg.message }}
-        </p>
-      </div>
+
+      <transition name="alert" type="animation">
+        <div 
+          id="alert-box" 
+          class="alert py-2 ml-3 mb-0 text-center" 
+          :class="alertStyles"
+          v-show="alertMsg.isAlert">
+          <p 
+            class="mb-0" 
+            id="alert-msg">
+            {{ alertMsg.message }}
+          </p>
+        </div>
+      </transition>
+
     </div>
   </div>
 
@@ -66,6 +81,16 @@
 <script>
   import { eventBus } from '../../main.js'
   import AddSteps from './AddSteps.vue'
+
+  function fadeChildren(el, opacity) {
+    //console.log(el)
+    const elemChildren = el.children
+    //console.log(children)
+    const children = Array.from(elemChildren)
+    children.forEach(child => {
+      child.style.opacity = opacity
+    })
+  }
 
   export default {
     data () {
@@ -97,16 +122,17 @@
           'alert-danger': !this.alertMsg.isSuccess
         }
       },
-      mediaQueries(){
-        // not quite finished! fix these! 
-        return {
-          // 'grid--mobile': this.$mq === 'mobile',
-          // 'grid': !this.mq === 'mobile'
-        }
-
-      }
     },
     methods: {
+      beforeEnter: (el) => {
+        fadeChildren(el, 0)
+      },
+      afterEnter: el => {
+        fadeChildren(el, 1)
+      },
+      beforeLeave: el => {
+        fadeChildren(el, 0)
+      },
       addNewTask() {
         if(this.newTask.task === '') {
           console.log('Task must have a title')
@@ -163,6 +189,34 @@
   display: flex;
 }
 
+.add-details-body {
+  max-height: 30rem;
+  height: auto;
+}
+
+.expand-enter {
+  max-height: 0;
+}
+.expand-enter-active {
+  transition: max-height .1s ease-in-out;
+}
+.expand-leave-active {
+  transition: max-height .1s ease-in-out;
+}
+.expand-leave-to {
+  max-height: 0;
+}
+
+
+.alert-enter-active {
+  animation: alert-in .3s ease-in-out forwards;
+}
+.alert-leave-active {
+  animation: alert-out .3s ease-in-out forwards;
+}
+
+
+
 .add-details-header {
   display: flex;
   flex: 1;
@@ -179,7 +233,6 @@
   grid-template-columns: 1fr 1fr;
   grid-gap: 1rem;
 }
-
 
 #add-new-step-btn {
   font-size: .9rem;
@@ -203,5 +256,38 @@
     grid-gap: 0rem;
   }
 }
+
+
+@keyframes alert-in {
+  from {
+    opacity: 0;
+    transform: scale(.8)
+  }
+  75% {
+    opacity: 1;
+    transform: scale(1.05)
+  }
+  to {
+    transform: scale(1)
+  }
+}
+@keyframes alert-out {
+  from {
+    opacity: 1;
+    transform: scale(1)
+  }
+  25% {
+    opacity: 1;
+    transform: scale(1.05)
+  }
+  to {
+    opacity: 0;
+    transform: scale(.8)
+  }
+}
+
+
+
+
 
 </style>
