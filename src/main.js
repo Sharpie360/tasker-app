f4d21c40eadd8ad563ca31cce7420e368397c615
SeanBecker <techtronixwebdesign@gmail.com>
2018-12-13 23:14:01 -0600

    updated - using store.js

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
-
-
+import store from './store'
 
 export const eventBus = new Vue({})
 
 new Vue({
+  store,
   el: '#app',
   render: h => h(App)
 })
